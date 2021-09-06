207e0251cc3993d508a9d2f2a2d0708b0f1d5028
cheng.ranran <cheng.ranran@immomo.com>
2021-09-06 20:41:41 +0800

    feat: ref+computed+reactive+toRefs

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,42 @@
 <template>
-  <div>
-    {{ count }}
+  <div id="app">
+    <div class="num">
+      {{ count }}
+      {{ double }}
+    </div>
+    <button class="add" @click="add">add</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, computed, reactive, toRefs } from 'vue';
+interface DataProps {
+  count: number;
+  add: () => void;
+  double: number;
+}
 
 export default defineComponent({
   setup() {
-    const count = ref(0);
-    // computed
+    // ref 定义数据   ---   返回
+    // const count = ref(0); // ref对象可以直接取count中的value值
+    // // 计算属性，是一个函数，传入一个回调函数参数   ----  返回
+    // const double = computed(() => {
+    //   return count.value * 2
+    // })
+    // // methods 直接定义箭头函数  ---  返回
+    // const add = () => {
+    //   count.value++;
+    // }
+    const data: DataProps = reactive({
+      count: 0,
+      add: () => { data.count++ },
+      double: computed(() => data.count * 2)
+    })
+
+    const refData = toRefs(data);
     return {
-      count
+      ...refData
     }
   }
 });
