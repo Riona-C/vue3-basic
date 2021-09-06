0aba22c988970bb48a0a1f359fe76f842dc92272
cheng.ranran <cheng.ranran@immomo.com>
2021-09-06 19:47:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    {{ count }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const count = ref(0);
+    // computed
+    return {
+      count
+    }
   }
 });
 </script>
